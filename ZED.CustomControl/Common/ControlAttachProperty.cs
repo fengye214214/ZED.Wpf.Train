d4534badf63334608a00522cc58d7b9e30926ce5
fengye <3@qq.com>
2017-11-03 01:30:56 +0800

    添加WindowBase,完成50%

---
+++ ZED.CustomControl/Common/ControlAttachProperty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ZED.CustomControl
+{
+    public static class ControlAttachProperty
+    {
+        #region border圆角
+
+
+
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ControlAttachProperty), new PropertyMetadata(null));
+
+
+
+        #endregion
+    }
+}
