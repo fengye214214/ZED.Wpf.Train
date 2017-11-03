d39ce252bab3b1f9925c27b0709e3c99e88c6ca1
封晔 <578797371@qq.com>
2017-11-03 08:35:33 +0800

    合并

--- ZED.Wpf.Train/Common/LanguageHelper.cs
+++ ZED.Wpf.Train/Common/LanguageHelper.cs
@@ -10,12 +10,12 @@ namespace ZED.Wpf.Train
     {
         public static void GenerateLangueXml()
         {
-            var model = new List<SysLangueViewModel>() 
-            {
-                new SysLangueViewModel(){ Langue = "ZH_CN", LangueRes = "ZH_CN.xaml" },
-                new SysLangueViewModel(){ Langue = "EN_US", LangueRes = "EN_US.xaml" },
-            };
-            XMLSerialize<List<SysLangueViewModel>>.SerializeAndSave(model, @"C:\Users\Administrator\Desktop\新建文件夹\ds.xml");
+            //var model = new List<SysLangueViewModel>() 
+            //{
+            //    new SysLangueViewModel(){ Langue = "ZH_CN", LangueRes = "ZH_CN.xaml" },
+            //    new SysLangueViewModel(){ Langue = "EN_US", LangueRes = "EN_US.xaml" },
+            //};
+            //XMLSerialize<List<SysLangueViewModel>>.SerializeAndSave(model, @"C:\Users\Administrator\Desktop\新建文件夹\ds.xml");
         }
     }
 }
